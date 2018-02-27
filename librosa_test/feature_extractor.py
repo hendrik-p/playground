b452b56e11ad40aae6172021a9aa2fb112e5cc22
hpesch <hendrikpesch@gmail.com>
2018-02-27 14:22:30 +0100

    Added automatic sample rate detection to MFCC feature extractor.

--- librosa_test/feature_extractor.py
+++ librosa_test/feature_extractor.py
@@ -1,10 +1,11 @@
 import numpy as np
+import wave
 from librosa.core import load
 from librosa.feature import mfcc
 
 class MfccFeatureExtractor(object):
 
-    def __init__(self, sample_rate=16000, 
+    def __init__(self, sample_rate=None, 
                        nb_mfcc=16, 
                        fft_size=0.025, 
                        window_shift=0.01, 
@@ -15,7 +16,16 @@ class MfccFeatureExtractor(object):
         self.window_shift = window_shift
         self.mean_normalize = mean_normalize
 
+    def detect_sample_rate(self, audio_path):
+        sample_rate = 0
+        audio_file = wave.open(audio_path)
+        sample_rate = audio_file.getframerate()
+        audio_file.close()
+        return sample_rate
+
     def extract_features(self, path):
+        if not self.sample_rate:
+            self.sample_rate = self.detect_sample_rate(path)
         y, sr = load(path, sr=self.sample_rate)
         n_fft = int(self.fft_size * self.sample_rate)
         hop_length = int(self.window_shift * self.sample_rate)
