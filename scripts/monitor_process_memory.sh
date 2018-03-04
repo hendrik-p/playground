4643c35575127ac81812568ae090cdb143acb030
Hendrik Pesch <hpesch@apptek.com>
2018-03-04 05:27:13 -0500

    Take RSS memory size instead of total.

--- scripts/monitor_process_memory.sh
+++ scripts/monitor_process_memory.sh
@@ -8,9 +8,9 @@ pid=$1
 export LC_NUMERIC="en_US.UTF-8" # printf would complain otherwise
 while [ -e /proc/$pid ];
 do
-  mem=$(pmap $pid | grep "total" | sed "s|  *| |g" | cut -d " " -f 3 | sed "s|K||g")
-  mem_in_gb=$(echo "$mem / (1024 * 1024)" | bc -l)
+  mem=$(pmap -x $pid | grep "total" | sed "s|  *| |g" | cut -d " " -f 4 | sed "s|K||g")
+  mem_in_gb=$(echo "$mem / (1000 * 1000)" | bc -l)
   mem_in_gb=0$mem_in_gb
-  printf "%0.2fG\n" $mem_in_gb
+  printf "%0.2fGb\n" $mem_in_gb
   sleep 1
 done
